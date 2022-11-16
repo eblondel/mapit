3f7400425490988455e0494238c0d59efa4a8439
eblondel <emmanuel.blondel1@gmail.com>
2022-11-16 16:55:41 +0100

    labels arg

--- R/create_flow_map.R
+++ R/create_flow_map.R
@@ -7,7 +7,7 @@
 create_flow_map <- function(sf, by, data, data.col.from, data.col.to, 
                             arrow.trunc = 20, arrow.col = "#C3E4E5",
                             target.highlight = TRUE, target.col = "#0088A2",
-                            label.box.col = "#0088A2", label.bg = "#C3E4E5"){
+                            labels = TRUE, label.box.col = "#0088A2", label.bg = "#C3E4E5"){
   
   if(!is(data,"data.frame")) stop("Data should of class 'data.frame'")
 
@@ -32,7 +32,7 @@ create_flow_map <- function(sf, by, data, data.col.from, data.col.to,
       col = arrow.col
     )
     sf.from.coords <- sf::st_coordinates(sf.from)
-    mapit::create_legend(sf.from.coords[1], sf.from.coords[2], paste0(data[i,]$value*100,"%"), text.col = "white", text.font = 2, 
+    if(labels) mapit::create_legend(sf.from.coords[1], sf.from.coords[2], paste0(data[i,]$value*100,"%"), text.col = "white", text.font = 2, 
                          box.col = label.box.col, bg = label.bg, xjust = 0.5, yjust = 0.5, x.intersp = 0)
     
   }
@@ -42,7 +42,7 @@ create_flow_map <- function(sf, by, data, data.col.from, data.col.to,
       plot(sf.to.pt, pch = "\u21BA", font = 2, col = "white", cex = 4, add = TRUE)
       
       sf.to.coords <- sf::st_coordinates(sf.to)
-      mapit::create_legend(sf.to.coords[1], sf.to.coords[2], paste0(data[i,]$value*100,"%"), text.col = "white", text.font = 2, 
+      if(labels) mapit::create_legend(sf.to.coords[1], sf.to.coords[2], paste0(data[i,]$value*100,"%"), text.col = "white", text.font = 2, 
                            box.col = label.box.col, bg = label.bg, xjust = 0.5, yjust = 1, x.intersp = 0)
     }
   }

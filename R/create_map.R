e4477d83cfaaf9ee4f4d50b421e44b26c7173dbb
eblondel <emmanuel.blondel1@gmail.com>
2023-07-26 12:04:04 +0200

    test specific halo for utf8 halfcircle symbols

--- R/create_map.R
+++ R/create_map.R
@@ -169,7 +169,7 @@ create_map <- function(sf = NULL, sfby = NULL, sfby.code = NULL,
       plot(fao_areas_lines, bg=bgCol, col=faoareasCol, add = TRUE)
     }
     sf_points <- sf::st_point_on_surface(sf)
-    if(halo){
+    if(halo) if(!is.character(pch)){
       plot(sf_points, lwd=halolwd, bg="transparent", col = halocol, pch = 21, cex = sf$CLASS*2.5, add = TRUE)
     }
     if(!is.null(plot.handler)){
@@ -187,6 +187,22 @@ create_map <- function(sf = NULL, sfby = NULL, sfby.code = NULL,
     }else{
       #with simple symbols
       plot(sf_points, lty=1, bg=col, col = col, pch = pch, cex = sf$CLASS*2.6, add = TRUE) 
+    }
+    
+    #case of proportional half circles
+    if(halo) if(is.character(pch)) if(pch %in% c("\u25D6","\u25D7")){
+      plot(do.call("rbind", lapply(1:nrow(sf_points),function(i){
+        buf_dist = (abs(par("usr")[3] * 1 / margin_comp_factor) - abs(graphics::grconvertY(sf_points[i,]$CLASS, "chars", "user")))/2
+        buf = sf::st_buffer(sf_points[i,], dist = buf_dist)
+        coords = sf::st_coordinates(sf_points)
+        buf_vertical_diameter = sf::st_linestring(rbind(c(coords[1,1], coords[1,2]-buf_dist),c(coords[1,1], coords[1,2]+buf_dist)))
+        buf_rect <- switch(pch,
+          "\u25D6" = sf::st_buffer(buf_vertical_diameter, dist = buf_dist, singleSide = TRUE),
+          "\u25D7" = sf::st_buffer(buf_vertical_diameter, dist = -buf_dist, singleSide = TRUE)
+        )
+        half_buffer = sf::st_intersection(buf, buf_rect)
+        return(half_buffer)
+      })), lty=1, bg="transparent", col = "transparent", lwd = halolwd, border = halocol, add = TRUE)
     }
     
     if(debug) plot(do.call("rbind", lapply(1:nrow(sf_points),function(i){
